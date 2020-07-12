e4093e4b409e047f591b60c6e5d9ef2995eea935
Priyanshu Mehta <mehta7priyanshu@gmail.com>
2020-07-12 21:38:17 +0530

    Day 12

--- Day 12/ReverseBits.java
+++ Day 12/ReverseBits.java
@@ -22,14 +22,14 @@ public class ReverseBits {
     }
 	
 	public int reverseBitsAlt(int n) {
-        int result = 0;
-    for (int i = 0; i < 32; i++) {
-        result += n & 1;
-        n >>>= 1;   // CATCH: must do unsigned shift
-        if (i < 31) // CATCH: for last digit, don't shift!
-            result <<= 1;
-    }
-    return result;
+            int result = 0;
+    	    for (int i = 0; i < 32; i++) {
+            result += n & 1;
+            n >>>= 1;   
+            if (i < 31) 
+                result <<= 1;
+            }
+        return result;
     }
 
 }
